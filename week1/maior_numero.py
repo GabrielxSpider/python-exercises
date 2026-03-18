1ff8ec1a1e2ebafe89521d0322880cae3900e173
Gabriel <gabrielxspider@gmail.com>
2026-03-18 15:39:37 -0300

    add comments to the python exercises + new exercise

--- week1/maior_numero.py
+++ week1/maior_numero.py
@@ -1,9 +1,13 @@
+# Define uma lista de números
 numeros = [3, 7, 2, 9, 5]
 
+# Pega o primeiro número pelo index
 maior = numeros[0]
 
+# Percorre a lista de números
 for n in numeros:
-    if n > maior:
-        maior = n
+    if n > maior:             # Checa se o número percorrido é maior que o atual maior
+        maior = n             # Se for, vira o novo maior número
 
+# Exibe o maior número encontrado
 print(maior)
