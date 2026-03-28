0bd8e4b49c3667bae7b0af55791a4b900446d659
Gabriel <gabrielxspider@gmail.com>
2026-03-28 16:02:25 -0300

    add week3 new exercise

---
+++ week3/while_loop_practice.py
@@ -0,0 +1,30 @@
+# Password input
+
+while True:
+    password = input("Enter a password: ")
+    if password == "python123":
+        print("Correct password. Logging in...")
+        break
+    else:
+        print("Incorrect password, try again.")
+
+# Count until stop
+
+while True:
+    number = input("Enter a number (type 0 to exit): ")
+    if number == "0":
+        break
+
+# Sum until exit
+
+total = 0
+
+while True:
+    print("Type -1 to see the final sum")
+    number = int(input("Enter a number to add: "))
+    if number == -1:
+        break
+
+total += number
+
+print("Total sum:", total)
