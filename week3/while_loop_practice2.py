d1dda67c4af882c5d4df05a2d72131220de32d3b
Gabriel <gabrielxspider@gmail.com>
2026-03-31 17:04:16 -0300

    add completed exercises

--- week3/while_loop_practice2.py
+++ week3/while_loop_practice2.py
@@ -1,11 +1,39 @@
 # simple menu
-import randint
-number = range(0,11)
+total = 0
+
 while True: 
-    print("1 - Say Hello!\n2 - Show random number\n3 - Leave")
-    choice = int(input(""))
-    if choice == 1:
-        print("Hello!")
-        continue
-    elif choice == 2:
-        print(randint(number))
+    print("1 - Say Hello!\n2 - Add number to sum\n3 - Show total sum\n4 - Leave")
+    choice = (input("-- Please insert a number: "))
+
+    if choice == "1":
+        print("Hello!\n")
+
+    elif choice == "2":
+        num = int(input("-- Input the number: ")) 
+        total += num
+
+    elif choice == "3":
+        print(total)
+
+    elif choice == "4":
+        print("Goodbye!")
+        break
+
+    else:
+        print("-- Insert a valid number please. --")
+
+# counter + sum
+
+total = 0
+quantity = 0
+
+while True:
+    digit = int(input("Please insert a number to sum, 0 to stop: "))
+
+    if digit == 0:
+        break
+
+    total += digit
+    quantity += 1
+
+print(f"The sum is {total} of {quantity} numbers.")
